47b3e0034d155f41d94fde91623a85b711eb465b
besta <bestasyusuf8@gmail.com>
2023-04-01 16:10:43 +0200

    Rabbitmq config

--- docker/rabbitmq-publisher/script.sh
+++ docker/rabbitmq-publisher/script.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
-#while :
-#do
-  SHIPMENT="{
-      \"shipmentId\": $(date +%s),
-      \"origin\": \"Frankfurt\",
-      \"destination\" : \"Hamburg\",
-      \"customerId\": \"customer$((1000 + $RANDOM % 10))\",
-      \"createdDate\": $(date +%s),
-      \"fragile\": true,
-      \"notifyCustomer\": true,
-      \"transportType\": \"AIR\",
-      \"temperature\": {
-          \"min\": -20,
-          \"max\": -10
-      }
-  }"
-  echo $SHIPMENT
-#	amqp-publish --server="${RABBITMQ_HOST}" --username="kn" --password="kn" --port=5672 --exchange="amq.fanout" --routing-key="shipment" --body="$SHIPMENT"
-#	sleep ${PUBLISH_FREQUENCY}
-#done
-
+while true; do
+    SHIPMENT="{\"shipmentId\": $(date +%s%N), \"origin\": \"Frankfurt\", \"destination\": \"Hamburg\", \"customerId\": \"customer$RANDOM\", \"createdDate\": $(date +%s%N), \"fragile\": true, \"notifyCustomer\": true, \"transportType\": \"AIR\", \"temperature\": { \"min\": -20, \"max\": -10 } }"
+    echo $SHIPMENT
+    amqp-publish --server="rabbitmq" --username="yusufbestas" --password="1234" --port=5672 --exchange="amq.fanout" --routing-key="shipment" --body="$SHIPMENT"
+    sleep $PUBLISH_FREQUENCY
+done
--- src/main/kotlin/com/kn/containershipment/ContainerShipmentApplication.kt
+++ src/main/kotlin/com/kn/containershipment/ContainerShipmentApplication.kt
@@ -1,12 +1,50 @@
 package com.kn.containershipment
 
+import com.rabbitmq.client.AMQP
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.DefaultConsumer
+import com.rabbitmq.client.Envelope
+import org.springframework.amqp.rabbit.annotation.EnableRabbit
+import org.springframework.context.annotation.ComponentScan
+
 
 @SpringBootApplication
+@EnableRabbit
+@ComponentScan(basePackages = ["com.kn"])
 class ContainerShipmentApplication
 
 
-fun main(args: Array<String>) {
-	runApplication<ContainerShipmentApplication>(*args)
+
+fun main() {
+	val factory = ConnectionFactory()
+	factory.host = "localhost"
+	factory.username = "yusufbestas"
+	factory.password = "1234"
+
+	val connection = factory.newConnection()
+	val channel = connection.createChannel()
+
+	val exchangeName = "amq.fanout"
+	val queueName = "shipment"
+	val routingKey = "shipment"
+
+	channel.exchangeDeclare(exchangeName, "fanout", true)
+	channel.queueDeclare(queueName, true, false, false, null)
+	channel.queueBind(queueName, exchangeName, routingKey)
+
+	val consumer = object : DefaultConsumer(channel) {
+		override fun handleDelivery(
+			consumerTag: String?,
+			envelope: Envelope?,
+			properties: AMQP.BasicProperties?,
+			body: ByteArray?
+		) {
+			val message = body?.let { String(it, charset("UTF-8")) }
+			println("Received message: $message")
+		}
+	}
+
+	channel.basicConsume(queueName, true, consumer)
 }
