1bfbce561611fbcf8b7f6c6836447866ef3201de
karaelf33 <bestasyusuf8@gmail.com>
2023-04-15 13:59:35 +0200

    the message send to queue in web page

--- docker/rabbitmq-publisher/script.sh
+++ docker/rabbitmq-publisher/script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+rabbitmqadmin declare queue --username=guest --password=guest --host="${RABBITMQ_HOST}" --port=15672 name=shipment
 
-while :
-do
-     SHIPMENT="{\"shipmentId\": $(date +%s%N), \"origin\": \"Frankfurt\", \"destination\": \"Hamburg\", \"customerId\": \"customer$RANDOM\", \"createdDate\": $(date +%s%N), \"fragile\": true, \"notifyCustomer\": true, \"transportType\": \"AIR\", \"temperature\": { \"min\": -20, \"max\": -10 } }"
+rabbitmqadmin declare binding --username=guest --password=guest --host="${RABBITMQ_HOST}" --port=15672 source=amq.fanout destination_type=queue destination=shipment routing_key=shipment
+
+
+while :; do
+  SHIPMENT="{\"shipmentId\": $(date +%s%N), \"origin\": \"Frankfurt\", \"destination\": \"Hamburg\", \"customerId\": \"customer$RANDOM\", \"createdDate\": $(date +%s%N), \"fragile\": true, \"notifyCustomer\": true, \"transportType\": \"AIR\", \"temperature\": { \"min\": -20, \"max\": -10 } }"
 
   echo $SHIPMENT
-	amqp-publish --server="${RABBITMQ_HOST}" --username="guest" --password="guest" --port=5672 --exchange="amq.fanout" --routing-key="shipment" --body="$SHIPMENT"
-	sleep ${PUBLISH_FREQUENCY}
+  amqp-publish --server="${RABBITMQ_HOST}" --username="guest" --password="guest" --port=5672 --exchange="amq.fanout" --routing-key="shipment" --body="$SHIPMENT"
+  sleep ${PUBLISH_FREQUENCY}
 done
