841e4f25768798d974dfa760c75fb3b7c592eb86
karaelf33 <bestasyusuf8@gmail.com>
2023-04-16 20:46:53 +0200

    selection shipment service created

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Shipment, TransportType} from "./models/shipment.mode";
 import {ShipmentService} from "./services/shared/shipment.service";
 import {PageEvent} from "@angular/material/paginator";
+import {ShipmentSelectionService} from "./services/shared/shipment-selection.service";
 
 @Component({
     selector: 'app-root',
@@ -20,7 +21,8 @@ export class AppComponent implements OnInit {
     pageIndex = 0;
     pageOptions: number[] = [5, 10, 25, 50];
 
-    constructor(private shipmentService: ShipmentService) {
+    constructor(private shipmentService: ShipmentService,
+                private shipmentSelectionService: ShipmentSelectionService) {
     }
 
     ngOnInit(): void {
@@ -40,5 +42,12 @@ export class AppComponent implements OnInit {
         })
     }
 
-    protected readonly event = event;
+    onShipmentSelected(shipment: Shipment, isChecked: boolean) {
+        if (isChecked) {
+            this.shipmentSelectionService.addSelectedShipment(shipment);
+        } else {
+            this.shipmentSelectionService.removeSelectedShipment(shipment);
+        }
+    }
+
 }
--- frontend/src/app/components/execution-plan-model/execution-plan-model.component.ts
+++ frontend/src/app/components/execution-plan-model/execution-plan-model.component.ts
@@ -1,19 +1,31 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Shipment, TransportType} from "../../models/shipment.mode";
+import {ShipmentSelectionService} from "../../services/shared/shipment-selection.service";
 
 @Component({
-  selector: 'execution-plan-model',
-  templateUrl: './execution-plan-model.component.html',
-  styleUrls: ['./execution-plan-model.component.scss']
+    selector: 'execution-plan-model',
+    templateUrl: './execution-plan-model.component.html',
+    styleUrls: ['./execution-plan-model.component.scss']
 })
 export class ExecutionPlanModelComponent implements OnInit {
 
-  @Input()
-  shipment: Shipment;
+    @Input()
+    shipment: Shipment;
+    selectedShipments: Shipment[] = [];
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+    constructor(private shipmentSelectionService: ShipmentSelectionService) {
+    }
 
+    ngOnInit(): void {
+    }
+
+    getSelectedShipments() {
+        this.selectedShipments = this.shipmentSelectionService.getSelectedShipments();
+    }
+
+    createExecutionPlan() {
+        this.selectedShipments = this.shipmentSelectionService.getSelectedShipments();
+
+    }
 }
